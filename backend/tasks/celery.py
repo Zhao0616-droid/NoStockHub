3080f71fd34fcb70bde2f5bf8db491510304aa31
FerryGeorge <1391240357@qq.com>
2026-05-23 00:45:16 +0800

    修改部署

--- backend/tasks/celery.py
+++ backend/tasks/celery.py
@@ -1,7 +1,7 @@
 from celery import Celery
 import os
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.base')
 
 app = Celery('project_platform')
 app.config_from_object('django.conf:settings', namespace='CELERY')
